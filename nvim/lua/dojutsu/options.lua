76a58b9a0896435e782daea45deab4b2ea54f6e2
Oskar Karlsson <oskar000202@gmail.com>
2023-11-26 11:48:45 +0100

    update

--- nvim/lua/dojutsu/options.lua
+++ nvim/lua/dojutsu/options.lua
@@ -3,10 +3,12 @@ local opt = vim.opt
 opt.tabstop=4
 opt.shiftwidth=4
 opt.autoindent=true
+opt.smartindent=true
 opt.number=true
 opt.relativenumber=true
 opt.smarttab=true
 opt.softtabstop=4
+
 
 -- Use system clipboard / WSL fix
 if vim.fn.has('wsl') == 1 then
